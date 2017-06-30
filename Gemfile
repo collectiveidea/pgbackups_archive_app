bda42083997023c86e1006590d5d61d87bd4f8ff
David Genord II <david@collectiveidea.com>
2017-06-30 15:49:51 -0400

    Massive refactor to stop using pgbackups-archive
    Some major chages. pgbackups-archive triggered a backup, downloaded that backup, and then uploaded to S3. This no longer uses that process and now relies on scheduled backups being setup on heroku. This then looks up the backups that have succeeded, checks what has already been pushed to S3, uploads anything that is missing.

--- Gemfile
+++ Gemfile
@@ -2,5 +2,8 @@ source "https://rubygems.org"
 
 ruby "2.3.4"
 
-gem "pgbackups-archive"
+gem "excon"
+gem "fog-aws"
 gem "platform-api"
+gem "rake"
+gem "rest-client"
--- lib/pg_backup_runner.rb
+++ lib/pg_backup_runner.rb
@@ -1,5 +1,10 @@
-require "pgbackups-archive"
+require "rest_client"
+require "excon"
 require "platform-api"
+require "fog/aws"
+require "json"
+require "time"
+require "tempfile"
 
 class PgBackupRunner
   attr_reader :args
@@ -13,34 +18,20 @@ class PgBackupRunner
   end
 
   def run
-    setup
-    backup
-    report_to_dms
-  end
-
-  def setup
-    ENV["HEROKU_API_KEY"] = args[:oauth_token] || ENV["OAUTH_TOKEN"]
-
-    # Configure the variables needed by pgbackups-archive
-    ENV["DATABASE_URL"]                    = app_env["DATABASE_URL"]
-    ENV["PGBACKUPS_APP"]                   = app_name
-    ENV["PGBACKUPS_URL"]                   = app_env["PGBACKUPS_URL"]
-    ENV["PGBACKUPS_AWS_ACCESS_KEY_ID"]     = app_env["PGBACKUPS_AWS_ACCESS_KEY_ID"]
-    ENV["PGBACKUPS_AWS_SECRET_ACCESS_KEY"] = app_env["PGBACKUPS_AWS_SECRET_ACCESS_KEY"]
-    ENV["PGBACKUPS_REGION"]                = app_env["PGBACKUPS_REGION"]
-    ENV["PGBACKUPS_BUCKET"]                = app_env["PGBACKUPS_BUCKET"]
-
-    if app_env["RAILS_ENV"]
-      # Setup cheater rails env for pg_backups gem
-      require File.expand_path("../fake_rails.rb", __FILE__)
-
-      Rails.env = app_env["RAILS_ENV"]
+    backups.select do |b|
+      b["succeeded"] &&
+      b["to_type"] == 'gof3r' &&
+      (Time.now - 1_209_600) < Time.parse(b["created_at"]) && # 2 weeks
+      s3_bucket.files.head(s3_file_key(b)).nil?
+    end.each do |b|
+      run_single(b)
     end
   end
 
-  # Run the archive command
-  def backup
-    PgbackupsArchive::Job.call
+  def run_single(backup = nil)
+    backup ||= backup_for(args[:num].to_i) if !(args[:num].nil? || args[:num].blank?)
+    backup ||= latest_backup
+    archive(backup) && report_to_dms
   end
 
   def report_to_dms
@@ -50,19 +41,97 @@ class PgBackupRunner
     Net::HTTP.get_response(URI(dms_url))
   end
 
+  def backups
+    @backups ||= JSON.parse(rest_client["transfers"].get).sort {|a,b| a["created_at"] <=> b["created_at"] }
+  end
+
+  def archive(backup)
+    return false if s3_bucket.files.head(s3_file_key(backup))
+
+    puts "Downloading #{app_name} #{backup["num"]}"
+
+    temp_file = Tempfile.new("pgbackup")
+    streamer = lambda do |chunk, remaining_bytes, total_bytes|
+      print "\r%0.2f%%" % [100 - remaining_bytes.to_f / total_bytes * 100]
+      temp_file.write chunk
+    end
+
+    # https://github.com/excon/excon/issues/475
+    Excon.get public_url(backup["num"]),
+      response_block: streamer,
+      omit_default_port: true
+
+    temp_file.rewind
+
+    puts "\nUploading #{app_name} #{backup["num"]}"
+    s3_bucket.files.create({
+      key: s3_file_key(backup),
+      body: temp_file,
+      public: false,
+      encryption: "AES256"
+    })
+
+    temp_file.delete
+
+    true
+  end
+
   protected
 
   # Connect to the Heroku Platform API
   # and retrieve the app config
   def app_env
-    @app_env ||= PlatformAPI.connect_oauth(ENV["HEROKU_API_KEY"]).config_var.info_for_app(app_name)
+    @_app_env ||= PlatformAPI.connect_oauth(heroku_api_key).config_var.info_for_app(app_name)
   end
 
   def app_name
     args[:app_name] || ENV["APP_NAME"]
   end
 
+  def backup_for(num)
+    backups.detect {|b| b["num"] == num } || raise("backup #{num} not found")
+  end
+
   def dms_url
     app_env["PGBACKUPS_DMS_URL"]
+  end
+
+  def heroku_api_key
+    args[:oauth_token] || ENV["OAUTH_TOKEN"]
+  end
+
+  def latest_backup
+    backups.select { |b| b["succeeded"] && b["to_type"] == 'gof3r' }.sort_by { |b| b["created_at"] }.last
+  end
+
+  def public_url(num)
+    JSON.parse(rest_client["transfers/#{num}/actions/public-url"].post("{}"))["url"]
+  end
+
+  def s3_bucket
+    @_s3_bucket ||= s3_connection.directories.get app_env["PGBACKUPS_BUCKET"]
+  end
+
+  def s3_connection
+    @_s3_connection ||= Fog::Storage.new({
+      :provider              => "AWS",
+      :aws_access_key_id     => app_env["PGBACKUPS_AWS_ACCESS_KEY_ID"],
+      :aws_secret_access_key => app_env["PGBACKUPS_AWS_SECRET_ACCESS_KEY"],
+      :region                => app_env["PGBACKUPS_REGION"],
+      :persistent            => false
+    })
+  end
+
+  def s3_file_key(backup)
+    timestamp = backup["created_at"].gsub(/\/|\:|\.|\s/, "-").concat(".dump")
+    ["pgbackups", app_env["RAILS_ENV"], timestamp].compact.join("/")
+  end
+
+  def rest_client
+    RestClient::Resource.new(
+      "https://postgres-api.heroku.com/client/v11/apps/#{app_name}",
+      :user => "",
+      :password => heroku_api_key
+    )
   end
 end
